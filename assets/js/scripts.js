3ddd8e62224e54eaf23ec5c6e0ec376b496bdc22
Igor Polezhaiev <rogodessa@gmail.com>
2022-03-24 23:03:18 +0200

    close btn & fix to-top btn

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,12 +1,22 @@
 (function($){
     var btn = $('#to-top');
     $(window).scroll(function() {
-        if ($(window).scrollTop() > 500) {
+        if ($(window).scrollTop() > 800) {
             btn.addClass('show');
+            topBtnPosition();
         } else {
             btn.removeClass('show');
         }
     });
+
+    function topBtnPosition() {
+        let height = 60;
+        let aff_bottom_cta = $('.aff-bottom-cta');
+        if( aff_bottom_cta.length && aff_bottom_cta.hasClass('is-visible') ) {
+            height = aff_bottom_cta.height() + 40;
+        }
+        btn.css({bottom: height});
+    }
 
     btn.on('click', function(e) {
         e.preventDefault();
@@ -15,10 +25,21 @@
 
     $( document ).ajaxComplete(function( event, xhr, settings ) {
         // reload after add review
-        console.log(xhr);
         if( settings.data && settings.data.indexOf('action=review_form') !== -1 ){
             window.location.reload();
         }
+    });
+
+    $('.aff-bottom-close').on('click', function(){
+        $('.aff-bottom-cta').remove();
+    });
+
+    $(window).on('resize', function(){
+        topBtnPosition();
+    });
+
+    $(document).on('click', function(){
+        topBtnPosition();
     });
 
 })(jQuery);
--- functions.php
+++ functions.php
@@ -29,44 +29,30 @@ add_action('wp_insert_post', function($post_ID, $post, $update){
             'ID' => $post_ID,
             'post_status' => 'publish'
         ]);
-
-        // clear Rocket cache for product, when add new comment
-        if( function_exists('rocket_clean_post') ) {
-            $product_id = get_post_meta($post_ID, 'review_product_id', true);
-            if( !empty($product_id) ) {
-                rocket_clean_post($product_id);
-            }
-        }
-
     }
 }, 10, 3);
 
 
-// clear Rocket all cache after update theme
-add_action( 'upgrader_process_complete', 'clear_rocket_all_cache_after_update_theme',10, 2);
-function clear_rocket_all_cache_after_update_theme( $upgrader_object, $options ) {
-    if ($options['action'] == 'update' && $options['type'] == 'theme' ) {
-        if( function_exists('rocket_clean_domain') ) {
-            rocket_clean_domain();
-        }
-    }
-}
-
-
-/*function update_on_reviews_saved( $post_id, $post, $update ) {
-
-    if ( $post->post_type != 'product-reviews' ) return;
-    if ( wp_is_post_revision( $post_id ) ) return;
-    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
-
-    remove_action('save_post', 'update_on_reviews_saved');
-
-    wp_update_post( [
-        'ID'           => $post_id,
-        'post_status' => 'publish'
-    ] );
-
-    // re-hook this function
-    add_action('save_post', 'update_on_reviews_saved');
-}
-add_action( 'save_post', 'update_on_reviews_saved', 50, 3 );*/
+//// clear Rocket cache for product, when add new comment
+//add_action('post_updated', 'clear_rocket_product_cache_after_add_review', 20, 2);
+//function clear_rocket_product_cache_after_add_review($post_ID, $post_after) {
+//    if( $post_after->post_type == 'product-reviews' ) {
+//        if (function_exists('rocket_clean_post')) {
+//            $product_id = get_post_meta($post_ID, 'review_product_id', true);
+//            if (!empty($product_id)) {
+//                rocket_clean_post($product_id);
+//            }
+//        }
+//    }
+//}
+//
+//
+//// clear Rocket all cache after update theme
+//add_action( 'upgrader_process_complete', 'clear_rocket_all_cache_after_update_theme',10, 2);
+//function clear_rocket_all_cache_after_update_theme( $upgrader_object, $options ) {
+//    if ($options['action'] == 'update' && $options['type'] == 'theme' ) {
+//        if( function_exists('rocket_clean_domain') ) {
+//            rocket_clean_domain();
+//        }
+//    }
+//}
