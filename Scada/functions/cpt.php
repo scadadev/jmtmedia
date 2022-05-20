fb6b67f0b0d7082f228afbf120a2fae0943e2ffa
Igor Polezhaiev <rogodessa@gmail.com>
2022-05-20 18:03:15 +0300

    fix bug wmpl languages switcher links

--- Scada/functions/cpt.php
+++ Scada/functions/cpt.php
@@ -20,21 +20,45 @@ add_filter('post_type_archive_link', function($link, $post_type){
 /** fix  */
 add_filter('wpml_alternate_hreflang', function($url, $lang_code){
 
-    if(is_post_type_archive('games')) {
+    if( $archive_url = get_cpt_archive_link($lang_code) ) {
+        return $archive_url;
+    }
+
+    return $url;
+}, 10, 2);
+
+
+add_filter( 'icl_ls_languages', function( $languages ) {
+    foreach( $languages as $key => $language ) {
+        if( !empty( $language['code'] ) ) {
+            if( $archive_url = get_cpt_archive_link($language['code']) ) {
+                $languages[$key]['url'] = $archive_url;
+            }
+        }
+    }
+    return $languages;
+});
+
+
+function get_cpt_archive_link($lang_code) {
+    if(is_post_type_archive(['games', 'product'])) {
+
+        $object = get_queried_object();
+
         global $born_options;
 
         $slug = $born_options['aff_games_archive_slug_' . $lang_code];
 
-        $post_type_obj = get_post_type_object( 'games' );
+        $post_type_obj = get_post_type_object( $object->name );
 
         $struct = ( true === $post_type_obj->has_archive ) ? $post_type_obj->rewrite['slug'] : $post_type_obj->has_archive;
 
         $link = get_site_url() .'/'. $lang_code .'/'. user_trailingslashit( $struct, 'post_type_archive' );
 
         if( !empty($slug) ) {
-            $url = str_replace( $struct, $slug, $link );
+            return str_replace( $struct, $slug, $link );
         }
     }
 
-    return $url;
-}, 10, 2);
+    return false;
+}
