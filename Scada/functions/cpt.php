7d6c196c014d38dc17fd3b04089415b60c4314aa
Igor Polezhaiev <rogodessa@gmail.com>
2022-05-20 14:55:59 +0300

    fix bug wmpl hreflang

--- Scada/functions/cpt.php
+++ Scada/functions/cpt.php
@@ -16,3 +16,26 @@ add_filter('post_type_archive_link', function($link, $post_type){
     return $link;
     
 }, 10, 2);
+
+
+/** fix  */
+add_filter('wpml_alternate_hreflang', function($url, $lang_code){
+
+    if(is_post_type_archive('games')) {
+        global $born_options;
+
+        $slug = $born_options['aff_games_archive_slug_' . $lang_code];
+
+        $post_type_obj = get_post_type_object( 'games' );
+
+        $struct = ( true === $post_type_obj->has_archive ) ? $post_type_obj->rewrite['slug'] : $post_type_obj->has_archive;
+
+        $link = get_site_url() .'/'. $lang_code .'/'. user_trailingslashit( $struct, 'post_type_archive' );
+
+        if( !empty($slug) ) {
+            $url = str_replace( $struct, $slug, $link );
+        }
+    }
+
+    return $url;
+}, 10, 2);
