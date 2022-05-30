91074e8d9b4a82d327802912243701448349e936
Igor Polezhaiev <rogodessa@gmail.com>
2022-05-30 19:51:35 +0300

    return sitemap

--- rank-math.php
+++ rank-math.php
@@ -7,12 +7,12 @@ add_filter( 'rank_math/sitemap/enable_caching', '__return_false');
 /**
  * Fix sitemap url
  */
-//add_filter( 'rank_math/sitemap/entry', function( $url, $type, $object ){
-//    if( $object->post_type === 'games' || $object->post_type === 'products' ) {
-//        $post_language_details = apply_filters( 'wpml_post_language_details', NULL, $object->ID );
-//        if( !empty($post_language_details['language_code']) && $new_url = get_cpt_single_link($post_language_details['language_code'], $object) ) {
-//            $url['loc'] = $new_url;
-//        }
-//    }
-//    return $url;
-//}, 10, 3 );
+add_filter( 'rank_math/sitemap/entry', function( $url, $type, $object ){
+    if( $object->post_type === 'games' || $object->post_type === 'products' ) {
+        $post_language_details = apply_filters( 'wpml_post_language_details', NULL, $object->ID );
+        if( !empty($post_language_details['language_code']) && $new_url = get_cpt_single_link($post_language_details['language_code'], $object) ) {
+            $url['loc'] = $new_url;
+        }
+    }
+    return $url;
+}, 10, 3 );
