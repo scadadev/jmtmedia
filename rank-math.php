eb290f2780b2c72cbcf137a071395e297bf2758e
Igor Polezhaiev <rogodessa@gmail.com>
2022-06-07 18:03:46 +0300

    fix home url on sitemap

--- rank-math.php
+++ rank-math.php
@@ -14,5 +14,20 @@ add_filter( 'rank_math/sitemap/entry', function( $url, $type, $object ){
             $url['loc'] = $new_url;
         }
     }
+    elseif($object->post_type === 'page' && function_exists('icl_object_id')) {
+        $post_language_details = apply_filters( 'wpml_post_language_details', NULL, $object->ID );
+        $frontpage_id = get_option('page_on_front');
+        $frontpage_id_for_lang = icl_object_id($frontpage_id, 'page', false, $post_language_details['language_code']);
+
+        if( $object->ID == $frontpage_id || $object->ID == $frontpage_id_for_lang ) {
+            $url = str_replace(
+                [
+                    $object->post_name . '/',
+                    $object->post_name
+                ],
+                '',
+                $url);
+        }
+    }
     return $url;
 }, 10, 3 );
